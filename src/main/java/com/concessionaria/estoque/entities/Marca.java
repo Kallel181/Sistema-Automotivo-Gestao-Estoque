fe9aa410933ebfce0bfba87c74685e74090036e6
Kallel181 <86331656+Kallel181@users.noreply.github.com>
2026-05-27 09:45:14 -0300

    Create Marca.java

---
+++ src/main/java/com/concessionaria/estoque/entities/Marca.java
@@ -0,0 +1,26 @@
+package com.concessionaria.estoque.entities;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "marcas")
+public class Marca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nomeMarca;
+
+    @OneToMany(mappedBy = "marca", cascade = CascadeType.ALL, lazy = FetchType.LAZY)
+    private List<Modelo> modelos;
+
+    public Marca() {}
+    public Marca(String nomeMarca) { this.nomeMarca = nomeMarca; }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNomeMarca() { return nomeMarca; }
+    public void setNomeMarca(String nomeMarca) { this.nomeMarca = nomeMarca; }
+}
